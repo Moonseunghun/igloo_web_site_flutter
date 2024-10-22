7f47edf58c2b978d423c2aaecd4cabb723bc20b3
moonseunghun <mangokkaksae2@gmail.com>
2024-10-22 21:57:56 +0900

    setting : web site

---
+++ lib/model/igloo_about_text/igloo_about_text.dart
@@ -0,0 +1,11 @@
+class iglooAboutText {
+  final String iglooText;
+  final String iglooSubText;
+  final String iglooFooterText;
+
+  iglooAboutText({
+    required this.iglooText ,
+    required this.iglooSubText,
+    required this.iglooFooterText
+  });
+}
